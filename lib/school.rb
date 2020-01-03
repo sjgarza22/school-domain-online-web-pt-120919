b26a8b80affbdc90f679644cd7269f35eb9b1c84
Sara Garza <sjgarza22@gmail.com>
2020-01-03 20:02:48 +0000

    Automatically backed up by Learn

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,17 @@
-# code here!
+class School
+  
+  ROSTER = {}
+  
+  def initialize(name)
+    @name = name
+  end
+  
+  def add_student(name, grade)
+    
+  end
+
+  def roster
+    ROSTER
+  end
+  
+end
