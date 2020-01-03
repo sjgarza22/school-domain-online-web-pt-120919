a74c57960bf4ab4d47951287d1e36ecd89354f84
Sara Garza <sjgarza22@gmail.com>
2020-01-03 20:06:50 +0000

    Automatically backed up by Learn

--- lib/school.rb
+++ lib/school.rb
@@ -7,7 +7,12 @@ class School
   end
   
   def add_student(name, grade)
-    
+    if ROSTER.has_key?(grade)
+      ROSTER[grade] << [name, grade]
+    else
+      ROSTER[grade] = []
+      ROSTER[grade] << [name, grade]
+    end
   end
 
   def roster
